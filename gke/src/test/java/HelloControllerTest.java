755bb542e6df3a00b728fd80a46b9796a1063dd4
craigdbarber <craigbarber@google.com>
2019-07-26 10:48:10 -0700

    further fail

--- gke/src/test/java/HelloControllerTest.java
+++ gke/src/test/java/HelloControllerTest.java
@@ -41,7 +41,7 @@ public class HelloControllerTest {
     @Test
     public void getHello() throws Exception {
       fail();
-      
+      fail();
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
